6b6b3587ab89f0d79e1c69529e1d43605d032b9b
Marcelo XP <marceloxp@gmail.com>
2023-08-08 13:51:44 -0300

    - sync.

--- app.py
+++ app.py
@@ -1,30 +1,22 @@
+import torch
 from langchain.chains import RetrievalQA
 from huggingface_hub import hf_hub_download
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.llms import HuggingFacePipeline, LlamaCpp
-from constants import CHROMA_SETTINGS, PERSIST_DIRECTORY
 from transformers import LlamaTokenizer, LlamaForCausalLM, pipeline
-import click
+from pathlib import Path
 
-from constants import CHROMA_SETTINGS
-
-def load_ggml_model(device_type, model_id, model_basename):
-    print("Using Llamacpp for GGML quantized models")
-    model_path = hf_hub_download(repo_id=model_id, filename=model_basename)
-    print(model_path)
-    max_ctx_size = 2048
-    kwargs = {
-        "model_path": model_path,
-        "n_ctx": max_ctx_size,
-        "max_tokens": max_ctx_size,
-    }
-    if device_type.lower() == "mps":
-        kwargs["n_gpu_layers"] = 1000
-    if device_type.lower() == "cuda":
-        kwargs["n_gpu_layers"] = 1000
-        kwargs["n_batch"] = max_ctx_size
-    return LlamaCpp(**kwargs)
+ROOT_DIRECTORY = Path().resolve()
+# Define the folder for storing database
+SOURCE_DIRECTORY = f"{ROOT_DIRECTORY}/documents"
+PERSIST_DIRECTORY = f"{ROOT_DIRECTORY}/db"
+# Define the Chroma settings
+CHROMA_SETTINGS = Settings(
+        chroma_db_impl='duckdb+parquet',
+        persist_directory=PERSIST_DIRECTORY,
+        anonymized_telemetry=False
+)
 
 def load_model():
     '''
@@ -32,17 +24,14 @@ def load_model():
     If you are running this for the first time, it will download a model for you. 
     subsequent runs will use the model from the disk. 
     '''
-    # model_id = "TheBloke/vicuna-7B-1.1-HF"
-    model_id = "TheBloke/orca_mini_3B-GGML"
+    model_id = "TheBloke/vicuna-7B-1.1-HF"
     tokenizer = LlamaTokenizer.from_pretrained(model_id)
-
     model = LlamaForCausalLM.from_pretrained(model_id,
-                                            #   load_in_8bit=True, # set these options if your GPU supports them!
-                                            #   device_map=1#'auto',
-                                            #   torch_dtype=torch.float16,
-                                            #   low_cpu_mem_usage=True
+                                              load_in_8bit=True,
+                                              device_map=1, #'auto'
+                                              torch_dtype=torch.float16,
+                                              low_cpu_mem_usage=True
                                               )
-
     pipe = pipeline(
         "text-generation",
         model=model, 
@@ -57,58 +46,34 @@ def load_model():
 
     return local_llm
 
-@click.command()
-@click.option('--device_type', default='cpu', help='device to run on, select gpu or cpu')
-def main(device_type, ):
-    # load the instructorEmbeddings
-    if device_type in ['cpu', 'CPU']:
-        device='cpu'
-    else:
-        device='cuda'
-
-    print(f"Running on: {device}")
-        
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl", model_kwargs={"device": device})
-    embeddings = HuggingFaceInstructEmbeddings(model_name="all-MiniLM-L6-v2", model_kwargs={"device": device})
-    # load the vectorstore
-    db = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
-    retriever = db.as_retriever()
-    # Prepare the LLM
-    # callbacks = [StreamingStdOutCallbackHandler()]
-    # load the LLM for generating Natural Language responses. 
+device_type = "cuda"
+print(f"Running on: {device_type}")
     
-    model_basename = "orca-mini-3b.ggmlv3.q4_0.bin"
-    model_id = "TheBloke/orca_mini_3B-GGML"
-    llm = load_ggml_model(device_type, model_id, model_basename)
+embeddings = HuggingFaceInstructEmbeddings(model_name="all-MiniLM-L6-v2", model_kwargs={"device": device_type})
+# load the vectorstore
+db = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
+retriever = db.as_retriever()
 
-    # model_id = "s3nh/TinyLLama-v0-GGML"
-    # model_basename = "TinyLLama-v0.ggmlv3.q8_0.bin"
-    # llm = load_ggml_model(device_type, model_id, model_basename)
+# load the LLM for generating Natural Language responses. 
+llm = load_model()
 
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
-    # Interactive questions and answers
-    while True:
-        query = input("\nEnter a query: ")
-        if query == "exit":
-            break
-        
-        # Get the answer from the chain
-        res = qa(query)    
-        answer, docs = res['result'], res['source_documents']
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
 
-        # Print the result
-        print("\n\n> Question:")
-        print(query)
-        print("\n> Answer:")
-        print(answer)
-        
-        # # Print the relevant sources used for the answer
-        print("----------------------------------SOURCE DOCUMENTS---------------------------")
-        for document in docs:
-            print("\n> " + document.metadata["source"] + ":")
-            print(document.page_content)
-        print("----------------------------------SOURCE DOCUMENTS---------------------------")
+query = "Como foram as vendas?"
 
+# Get the answer from the chain
+res = qa(query)    
+answer, docs = res['result'], res['source_documents']
 
-if __name__ == "__main__":
-    main()
+# Print the result
+print("\n\n> Question:")
+print(query)
+print("\n> Answer:")
+print(answer)
+
+# # Print the relevant sources used for the answer
+print("----------------------------------SOURCE DOCUMENTS---------------------------")
+for document in docs:
+    print("\n> " + document.metadata["source"] + ":")
+    print(document.page_content)
+print("----------------------------------SOURCE DOCUMENTS---------------------------")
--- ingest.py
+++ ingest.py
@@ -1,13 +1,23 @@
 import os
-import click
 from typing import List
-
 from langchain.document_loaders import TextLoader, PDFMinerLoader, CSVLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.docstore.document import Document
-from constants import CHROMA_SETTINGS, SOURCE_DIRECTORY, PERSIST_DIRECTORY
 from langchain.embeddings import HuggingFaceInstructEmbeddings
+from chromadb.config import Settings
+from pathlib import Path
+
+ROOT_DIRECTORY = Path().resolve()
+# Define the folder for storing database
+SOURCE_DIRECTORY = f"{ROOT_DIRECTORY}/documents"
+PERSIST_DIRECTORY = f"{ROOT_DIRECTORY}/db"
+# Define the Chroma settings
+CHROMA_SETTINGS = Settings(
+        chroma_db_impl='duckdb+parquet',
+        persist_directory=PERSIST_DIRECTORY,
+        anonymized_telemetry=False
+)
 
 def load_single_document(file_path: str) -> Document:
     # Loads a single document from a file path
@@ -24,30 +34,20 @@ def load_documents(source_dir: str) -> List[Document]:
     all_files = os.listdir(source_dir)
     return [load_single_document(f"{source_dir}/{file_path}") for file_path in all_files if file_path[-4:] in ['.txt', '.pdf', '.csv'] ]
 
-@click.command()
-@click.option('--device_type', default='cpu', help='device to run on, select gpu or cpu')
-def main(device_type, ):
-    # load the instructorEmbeddings
-    if device_type in ['cpu', 'CPU']:
-        device='cpu'
-    else:
-        device='cuda'
+device_type = "cuda"
+# load the instructorEmbeddings
+print(f"Running on: {device_type}")
+# Load documents and split in chunks
+print(f"Loading documents from {SOURCE_DIRECTORY}")
+documents = load_documents(SOURCE_DIRECTORY)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+texts = text_splitter.split_documents(documents)
+print(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
+print(f"Split into {len(texts)} chunks of text")
 
-    # Load documents and split in chunks
-    print(f"Loading documents from {SOURCE_DIRECTORY}")
-    documents = load_documents(SOURCE_DIRECTORY)
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    texts = text_splitter.split_documents(documents)
-    print(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
-    print(f"Split into {len(texts)} chunks of text")
+# Create embeddings
+embeddings = HuggingFaceInstructEmbeddings(model_name="all-MiniLM-L6-v2", model_kwargs={"device": device_type})
 
-    # Create embeddings
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl", model_kwargs={"device": device})
-    embeddings = HuggingFaceInstructEmbeddings(model_name="all-MiniLM-L6-v2", model_kwargs={"device": device})
-    
-    db = Chroma.from_documents(texts, embeddings, persist_directory=PERSIST_DIRECTORY, client_settings=CHROMA_SETTINGS)
-    db.persist()
-    db = None
-
-if __name__ == "__main__":
-    main()
+db = Chroma.from_documents(texts, embeddings, persist_directory=PERSIST_DIRECTORY, client_settings=CHROMA_SETTINGS)
+db.persist()
+db = None
