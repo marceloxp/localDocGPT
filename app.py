0bae8a49cd91713c67d84c73ba5d1372a47a9781
Marcelo XP <marceloxp@gmail.com>
2023-08-08 16:25:21 -0300

    - Sync.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.llms import HuggingFacePipeline, LlamaCpp
 from transformers import LlamaTokenizer, LlamaForCausalLM, pipeline
+from chromadb.config import Settings
 from pathlib import Path
 
 ROOT_DIRECTORY = Path().resolve()
@@ -46,6 +47,23 @@ def load_model():
 
     return local_llm
 
+def load_ggml_model(device_type, model_id, model_basename):
+    print("Using Llamacpp for GGML quantized models")
+    model_path = hf_hub_download(repo_id=model_id, filename=model_basename)
+    print(model_path)
+    max_ctx_size = 2048
+    kwargs = {
+        "model_path": model_path,
+        "n_ctx": max_ctx_size,
+        "max_tokens": max_ctx_size,
+    }
+    if device_type.lower() == "mps":
+        kwargs["n_gpu_layers"] = 1000
+    if device_type.lower() == "cuda":
+        kwargs["n_gpu_layers"] = 1000
+        kwargs["n_batch"] = max_ctx_size
+    return LlamaCpp(**kwargs)
+
 device_type = "cuda"
 print(f"Running on: {device_type}")
     
@@ -55,7 +73,11 @@ db = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embeddings, 
 retriever = db.as_retriever()
 
 # load the LLM for generating Natural Language responses. 
-llm = load_model()
+# llm = load_model()
+
+model_id="TheBloke/Llama-2-7B-Chat-GGML"
+model_basename = "llama-2-7b-chat.ggmlv3.q4_0.bin"
+llm = load_ggml_model(device_type, model_id, model_basename)
 
 qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
 
